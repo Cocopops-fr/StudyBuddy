6696454ad9e50ccf8100d3533d9f0fa569b6e319
Leo <36920987+Cocopops-fr@users.noreply.github.com>
2025-11-29 02:23:12 +0100

    Remove non utilisé statement

--- user-service/src/main/java/com/studybuddy/users/UserServiceApplication.java
+++ user-service/src/main/java/com/studybuddy/users/UserServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@org.springframework.boot.web.server.servlet.context.ServletComponentScan
+//@org.springframework.boot.web.server.servlet.context.ServletComponentScan
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
--- user-service/src/main/java/com/studybuddy/users/UsersApplication.java
+++ user-service/src/main/java/com/studybuddy/users/UsersApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@org.springframework.boot.web.server.servlet.context.ServletComponentScan
+//@org.springframework.boot.web.server.servlet.context.ServletComponentScan
 public class UsersApplication {
     public static void main(String[] args) {
         SpringApplication.run(UsersApplication.class, args);
